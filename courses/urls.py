06b4039e38ab78c05b3d1982cdd483976d375b19
jkleczar <jkleczar@gmail.com>
2013-03-06 14:09:47 +0000

    course urls

--- courses/urls.py
+++ courses/urls.py
@@ -5,15 +5,31 @@ from courses.models import Course
 urlpatterns = patterns('',
 #   url(r'^$', views.index, name='index'),
 #    url(r'^(?P<courses_id>\d+)/$', views.detail, name='detail'),
-    url(r'^$',
+    url(r'^courselist',
         ListView.as_view(
-            queryset=Course.objects.order_by('-name')[:5],
+            queryset=Course.objects.order_by('-name')[:20],
             context_object_name='latest_courses_list',
-            template_name='courses/index.html'),
-        name='index'),
+            template_name='courses/courselist.html'),
+        name='courselist'),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Course,
             template_name='courses/detail.html'),
         name='detail'),
+    url(r'^$',
+        ListView.as_view(
+            queryset=Course.objects.order_by('-name')[:2],
+            context_object_name='latest_courses_list',
+            template_name='courses/index.html'),
+        name='index'),
+    url(r'^faq',
+        ListView.as_view(
+			model=Course,
+            template_name='courses/faq.html'),
+        name='faq'),
+    url(r'^contact',
+        ListView.as_view(
+			model=Course,
+            template_name='courses/contact.html'),
+        name='contact'),
 )
